282cb60205d6a10610127acdb360bfc95286a10b
Calvin <calvin.keats@gmail.com>
2021-01-31 22:59:40 -0600

    Added Illegal Stages to bin instead of manually editing

--- build-netplay.bat
+++ build-netplay.bat
@@ -3,4 +3,5 @@ echo Building netplay.json...
 gecko build -c netplay.json -defsym "STG_EXIIndex=1"
 echo.
 
-pause
+xcopy C:\Users\Calvin\Documents\prog\slippi-ssbm-asm\Output\Netplay\GALE01r2.ini C:\Users\Calvin\Documents\prog\slippi-online\Binary\x64\Sys\GameSettings /Y
+xcopy C:\Users\Calvin\Documents\prog\slippi-ssbm-asm\Output\Netplay\GALJ01r2.ini C:\Users\Calvin\Documents\prog\slippi-online\Binary\x64\Sys\GameSettings /Y
