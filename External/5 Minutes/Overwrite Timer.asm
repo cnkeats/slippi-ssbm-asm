432c17c2dc02ea90979236399690c8d137e2466b
Calvin <calvin.keats@gmail.com>
2022-04-11 17:47:25 -0500

    Add 5 Minutes

---
+++ External/5 Minutes/Overwrite Timer.asm
@@ -0,0 +1,32 @@
+################################################################################
+# Address: 8016e750
+################################################################################
+.include "Common/Common.s"
+.include "Online/Online.s"
+
+  # Check for online modes
+  lbz r17, OFST_R13_ONLINE_MODE(r13)
+  cmpwi r17, ONLINE_MODE_RANKED
+  beq Exit
+  cmpwi r17, ONLINE_MODE_UNRANKED
+  beq Exit
+  cmpwi r17, ONLINE_MODE_TEAMS
+  beq Exit
+
+  getMinorMajor r17
+  cmpwi r17, 0x0208
+  beq EditRules
+
+  # Check if Playback mode
+  cmpwi r17, 0x010E
+  beq EditRules
+
+  b Exit
+
+EditRules:
+  li r18, 300 # 5 Minutes
+  load r20, 0x80480540 # Time limit
+  stw r18, 0 (r20)
+
+Exit:
+  lis r4, 0x8017
