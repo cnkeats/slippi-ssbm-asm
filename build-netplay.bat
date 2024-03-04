06d2f5f538584560dbedac39f780f4ff11d84ce7
Calvin Keats <calvin.keats@gmail.com>
2024-03-04 15:17:06 -0600

    dev bat

--- build-netplay.bat
+++ build-netplay.bat
@@ -3,4 +3,6 @@ echo Building netplay.json...
 gecko build -c netplay.json -defsym "STG_EXIIndex=1" -batched
 echo.
 
-pause
+xcopy /y C:\Users\Owner\Documents\prog\slippi-ssbm-asm\Output\Netplay\* C:\Users\Owner\Documents\melee\test_dolphin\Sys\GameSettings\*
+xcopy /y C:\Users\Owner\Documents\prog\slippi-ssbm-asm\Output\Netplay\* C:\Users\Owner\Documents\melee\dev_dolphin\Sys\GameSettings\*
+xcopy /y C:\Users\Owner\Documents\prog\slippi-ssbm-asm\Output\Netplay\* C:\Users\Owner\Documents\prog\slippi-dolphin\Binary\x64\Sys\GameSettings\*
